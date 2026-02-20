d3c1a9c66b7481a2606e1afce4de92aac4f9db33
Vinicius Moura <vinijmoura@hotmail.com>
2026-02-20 19:58:55 +0000

    Add ncc build script and bundled dist/index.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const getJoke = require("./joke");
-const core = require("@actions/core");
 
 async function run() {
+  const core = await import("@actions/core");
   const joke = await getJoke();
   console.log(joke);
   core.setOutput("joke", joke);
